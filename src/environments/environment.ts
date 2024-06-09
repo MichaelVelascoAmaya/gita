7b0c15b052896dd0ed55384e8b764211dd88a6f6
Michael Alexander Velasco Amaya <michael.velasco.amaya@gmail.com>
2024-06-09 13:00:25 -0500

    add environments (manual)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  envName: 'local',
+  firebase: {
+    apiKey: "AIzaSyA_QfIfE3QYB0npNXcoNK9z4JdjSYROVy4",
+    authDomain: "gita-dev-project.firebaseapp.com",
+    projectId: "gita-dev-project",
+    storageBucket: "gita-dev-project.appspot.com",
+    messagingSenderId: "390661496459",
+    appId: "1:390661496459:web:43b99be451642858bcfe8e",
+    measurementId: "G-ZNR706HVL9"
+  }
+};

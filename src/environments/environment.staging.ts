b4852f6d5b7bda284f508d47a28c79ce873d7366
Michael Alexander Velasco Amaya <michael.velasco.amaya@gmail.com>
2024-06-09 17:20:56 -0500

    configure firebase environments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  envName: 'production',
+  envName: 'staging',
   firebase: {
     apiKey: "AIzaSyA_QfIfE3QYB0npNXcoNK9z4JdjSYROVy4",
     authDomain: "gita-dev-project.firebaseapp.com",
     projectId: "gita-dev-project",
     storageBucket: "gita-dev-project.appspot.com",
     messagingSenderId: "390661496459",
-    appId: "1:390661496459:web:43b99be451642858bcfe8e",
-    measurementId: "G-ZNR706HVL9"
+    appId: "1:390661496459:web:cf08bfd4a982c5dfbcfe8e",
+    measurementId: "G-HWM0KQTSGV"
   }
 };
